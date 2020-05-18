8aa5ae7c0d8beafdf090b73776ca6e2bad29afda
vadosl <hyperion.spb@gmail.com>
2020-05-18 15:30:20 +0300

    Updates

---
+++ precache-manifest.67d5f65471a151d8d3aa2770bc4c437b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "297611c3a7769b158ea5eec955353330",
+    "url": "/learning-cards/index.html"
+  },
+  {
+    "revision": "ce8eca970f9dd03769ba",
+    "url": "/learning-cards/static/css/main.9b3a90df.chunk.css"
+  },
+  {
+    "revision": "2d1b1b76457d2d40ab72",
+    "url": "/learning-cards/static/js/2.1d19692b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learning-cards/static/js/2.1d19692b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8eca970f9dd03769ba",
+    "url": "/learning-cards/static/js/main.8d117eea.chunk.js"
+  },
+  {
+    "revision": "88e9c050939fc09cb3c5",
+    "url": "/learning-cards/static/js/runtime-main.718bb8ec.js"
+  },
+  {
+    "revision": "898d65d07b3ff005f2afa1f917b5f6da",
+    "url": "/learning-cards/static/media/background.898d65d0.jpg"
+  },
+  {
+    "revision": "f387ab5c713c3506747b70004c048bbf",
+    "url": "/learning-cards/static/media/background2.f387ab5c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learning-cards/static/media/logo.5d5d9eef.svg"
+  }
+]);
